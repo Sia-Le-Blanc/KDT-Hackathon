8858f387f694eee43cab21d7069f49ed031f57e7
Sia Le Blanc <62240092+Sia-Le-Blanc@users.noreply.github.com>
2025-07-02 09:08:46 +0900

    7월 2일 작업폴더구성  수정함

--- backend/src/main/java/com/metamate/metaworkmate/MetaworkmateApplication.java
+++ backend/src/main/java/com/metamate/metaworkmate/MetaworkmateApplication.java
@@ -2,6 +2,6 @@ package main.java.com.metamate.metaworkmate;
 
 public class MetaworkmateApplication {
     public static void main(String[] args){
-        System.out.println("Hwllom world!");
+        System.out.println("Hello world!");
     }
 }
